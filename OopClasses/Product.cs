82d9b4bac3f6b2d67d7235f7ea8dccdf3af6143b
Sabir A. Asgarov <sabirsas@code.local>
2026-02-10 17:41:46 +0400

    Classwork

---
+++ OopClasses/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopClasses
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Count;
+
+        public Product() { }
+        public Product(string name, double price, int count) 
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+
+        public void Detail()
+        {
+            Console.WriteLine($"Product name: {Name}\nProduct price: {Price}\nProduct count: {Count}");
+        }
+
+        public double Discount(double discountPercent)
+        {
+            double discountedPrice = Price - ((Price * discountPercent) / 100);
+            return discountedPrice;
+        }
+
+    }
+}
---
+++ OopClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OopClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product book = new Product("Dune Messiah", 1000, 45987);
+
+            book.Detail();
+            Console.WriteLine(book.Discount(10.10));
+
+        }
+    }
+}
